e975d27c56d93ad0d1ecd6065cd83838054903c4
Paramesh <parames.babu@gmail.com>
2022-04-18 23:10:33 +0100

    Spring boot app for Azure DevOps learning

---
+++ src/main/java/com/uk/demo/SpringBootAzureDevOpsApplication.java
@@ -0,0 +1,16 @@
+package com.uk.demo;
+
+import com.uk.demo.restapi.SpringBootAzureWebController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackageClasses = SpringBootAzureWebController.class)
+public class SpringBootAzureDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureDevOpsApplication.class, args);
+	}
+
+}
